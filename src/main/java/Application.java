5a0794e79f8bcb18b2d0613e5dae9d5a5c620ede
kelvinleong <kelvin_liang885@hotmail.com>
2019-09-07 00:23:31 +0800

    Add graph and dfs algo

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,17 @@
+import algorithms.Graph;
+
 public class Application {
+    private static void graph() {
+        Graph g = new Graph();
+        g.addEdge(2, 1);
+        g.addEdge(2, 5);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(5, 1);
+        System.out.println("if it's reachable from 2 -> 5 == " + g.reachable(2, 5));
+    }
+
+    public static void main(String[] args) {
+        graph();
+    }
 }
--- src/main/java/algorithms/Graph.java
+++ src/main/java/algorithms/Graph.java
@@ -2,10 +2,8 @@ package algorithms;
 
 import designPattern.creational.Tuple;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Graph {
     private List<Tuple<Integer, List<Integer>>> graph = new ArrayList<>();
@@ -14,20 +12,47 @@ public class Graph {
         graph.stream()
                 .filter(t -> t.getFirst() == source)
                 .findFirst()
-                .map(t -> t.getSecond().add(dest))
+                .map(v -> v.getSecond().add(dest))
                 .or(() -> {
-                    graph.add(Tuple.Builder.newInstance().first(source).second(Arrays.asList(dest)).build());
-                    return Optional.of(false);
+                    List<Integer> dests = new ArrayList<>();
+                    dests.add(dest);
+                    graph.add(Tuple.Builder.newInstance().first(source).second(dests).build());
+                    return Optional.empty();
                 });
     }
 
-    public void traverse(int verticle, List<List<Integer>> list, int index) {
-        graph.forEach(g -> {
-            if (g.getFirst() == verticle) {
-                g.getSecond().forEach(v -> {
-                    list.get(index).add(v);
+    private void dfs(int s, int d, Map<Integer, Boolean> visited, AtomicBoolean result) {
+        if (visited.getOrDefault(s, true)) {
+            return;
+        }
+
+        graph.forEach(ve -> {
+            if (ve.getFirst() == s) {
+                visited.put(s, true);
+                ve.getSecond().forEach(dst -> {
+                    if (dst == d) result.getAndSet(true);
+                    System.out.println("v" + dst);
+                    dfs(dst, d, visited, result);
                 });
             }
         });
+    }
+
+    private Map<Integer, Boolean> visited() {
+        Map<Integer, Boolean> visited = new HashMap<>();
+        graph.forEach(v -> {
+            visited.put(v.getFirst(), false);
+            v.getSecond().forEach(s -> visited.put(s, false));
+        });
+
+        return visited;
+    }
+
+    public Boolean reachable(int s, int d) {
+        var visited = visited();
+        var result = new AtomicBoolean(false);
+        dfs(s, d, visited, result);
+
+        return result.get();
     }
 }
