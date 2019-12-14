0220e110b724d9c725bc4f9ced2f00154aa3c98e
kelvinleong <kelvin_liang885@hotmail.com>
2019-12-14 22:55:12 +0800

    remove trash codes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,4 @@
-import algorithms.MergeStrings;
 
 public class Application {
-
-    private static void mergeStrings() {
-        MergeStrings m = new MergeStrings();
-        String r = m.merge(new String[]{"apple", "iphonex"});
-        System.out.println("Result:: " + r);
-    }
-
-    public static void main(String[] args) {
-        mergeStrings();
-    }
+    public static void main(String[] args) {}
 }
