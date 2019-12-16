2fccd5b211e32bf9bc55636e9c4c9d83792c720d
kelvinleong <kelvin_liang@outlook.com>
2019-12-16 17:51:42 +0800

    *add a console for local programming

---
+++ src/main/java/leetcodeChallenge/Console.java
@@ -0,0 +1,10 @@
+package leetcodeChallenge;
+
+/**
+ *   for local programming and test, sometimes just cannot remember the syntax
+ */
+public class Console {
+    public void solution() {
+
+    }
+}
