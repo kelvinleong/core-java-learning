b0aba10f3123068b24fcba3987cfadc0e85f6632
kelvinleong <kelvin_liang885@hotmail.com>
2019-12-18 00:24:02 +0800

    *binary tree order level deep understanding

--- src/main/java/algorithms/BinaryTree.java
+++ src/main/java/algorithms/BinaryTree.java
@@ -22,16 +22,11 @@ public class BinaryTree {
     }
 
     private void levelTraverse(TreeNode root, int level, List<Integer> completedBinaryTree) {
-        if (root == null) {
-            completedBinaryTree.add(null);
-            return;
-        }
-
         if (level == 1) {
-            completedBinaryTree.add(root.val);
+            completedBinaryTree.add(root == null ? null: root.val);
         } else {
-            levelTraverse(root.left, level - 1, completedBinaryTree);
-            levelTraverse(root.right, level - 1, completedBinaryTree);
+            levelTraverse(root == null ? null : root.left, level - 1, completedBinaryTree);
+            levelTraverse(root == null ? null : root.right, level - 1, completedBinaryTree);
         }
     }
 
@@ -52,18 +47,27 @@ public class BinaryTree {
      *   #1 FIFO (using a queue) way
      */
     public void fifoTraverse(TreeNode root) {
+        int height = height(root);
+        int maxNoOfNodes = (int) Math.pow(2, height) - 1;
         List<Integer> tree = new ArrayList<>();
         Deque<TreeNode> q = new LinkedList<>();
         q.add(root);
         while (!q.isEmpty()) {
             TreeNode head = q.poll();
-            if (head != null) {
-                tree.add(head.val);
-                if (head.left != null) q.add(head.left);
-                if (head.right != null) q.add(head.right);
-            } else {
-                tree.add(null);
+            tree.add(head == null ? null : head.val);
+            if (tree.size() < maxNoOfNodes) {
+                q.add(head == null ? null : head.left);
+                q.add(head == null ? null : head.right);
             }
+        }
+    }
+
+    public void initTree(List<Integer> l) {
+        TreeNode root = new TreeNode(l.get(0));
+        TreeNode head = root;
+        for (int i = 0; i <= l.size()/ 2 - 1; ++i) {
+            head.left = new TreeNode(l.get(2 * i));
+            head.right = new TreeNode(l.get(2 * i + 1));
         }
     }
 }
