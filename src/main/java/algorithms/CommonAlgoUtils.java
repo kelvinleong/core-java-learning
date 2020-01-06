8c3080b49f171ccfd85cee201de5fcaa33a5b019
kelvinleong <kelvin_liang@outlook.com>
2020-01-06 16:36:31 +0800

    * algorithm trick if pattern string can be shifted to be a main string

--- src/main/java/algorithms/CommonAlgoUtils.java
+++ src/main/java/algorithms/CommonAlgoUtils.java
@@ -10,6 +10,21 @@ public class CommonAlgoUtils {
     public int getRightMostSetBit(int num) {
         return num & -num;
     }
+    
+    /**
+     *  Rotate pattern bit by bit and see if it can be m
+     *  The magic behind is that, say we have a main String m = t1t2....tm
+     *  Then we have a pattern String p = t2t3t4...tmt1
+     *  Duplicate m, it becomes t1t2.....tmt1....tm
+     *  Therefore, t2t3...tmt1 must bue found in the duplicated main string!
+     *
+     *  @param m (the main string)
+     *  @param pattern  (pattern String, if pattern can be shifted to be a m)
+     *  @return if pattern can be shifted so that it becomes m
+     */
+    public boolean checkIfRepeatedPattern(String m, String pattern) {
+        return m.equals(pattern) || (m + m).contains(pattern);
+    }
 
     /**
      *
