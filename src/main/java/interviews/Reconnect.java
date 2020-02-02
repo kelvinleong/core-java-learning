e8e0d5f758f123aea8955a14ed0f54861c5bf921
kelvinleong <kelvin_liang@outlook.com>
2020-02-02 14:27:33 +0800

    * Reconnect Interview

---
+++ src/main/java/interviews/Reconnect.java
@@ -0,0 +1,27 @@
+package interviews;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Reconnect {
+    /**
+     *  Given an array list, increment the duplicated elements so that they make the array unique
+     *  with minimum times and return the sum of the unique array
+     *  e.g., arr[1, 2, 2, 3] increment the third element to 4 so that the array becomes [1, 2, 4, 3]
+     *  which is unique and return the sum = 1 + 2 + 4 + 3 = 10
+     *  @param array
+     */
+    public Integer solve(List<Integer> array) {
+        array = array.stream().sorted().collect(Collectors.toList());
+
+        int prev = array.get(0);
+        int sum = array.get(0);
+        for (int i = 0; i < array.size(); ++i) {
+            if (array.get(i) <= prev) ++prev;
+            else prev = array.get(i);
+            sum += prev;
+        }
+
+        return sum;
+    }
+}
