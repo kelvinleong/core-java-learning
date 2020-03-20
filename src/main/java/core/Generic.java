3278669ce9616e3f75bff37fb2854faa62f20812
kelvinleong <kelvin_liang@outlook.com>
2020-03-20 11:09:10 +0800

    * generic

---
+++ src/main/java/core/Generic.java
@@ -0,0 +1,57 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Generic {
+    private class Animal {
+        private String name;
+
+        Animal(String name) {
+            this.name = name;
+        }
+
+        String getName() {
+            return this.name;
+        }
+    }
+
+    private class Cat extends Animal {
+        private String name;
+
+        Cat(String name) {
+            super(name);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private List<Cat> getCats() {
+        return List.of(new Cat("American Short Hair"),
+                        new Cat("British Short Hair"));
+    }
+
+    private List<Animal> getAnimals() {
+        return new ArrayList<>(Arrays.asList(new Cat("American Short Hair"),
+                new Cat("British Short Hair")));
+    }
+
+    /**
+     **   CSPE (Consumer Super and Producer Extend principle)
+     **/
+    public void generic() {
+        // PE principle
+        List<? extends Animal> animals1 = getCats();
+        animals1.forEach(a -> System.out.println(a.getName()));
+        // animals1.add(new Cat("meow"));  <-- this line will cause compilation error
+
+        // CS principle
+        List<? super Cat> animals2 = getAnimals();
+        animals2.add(new Cat("Siam Hairless"));
+        // animals2.forEach(a -> System.out.println(a.getName())); <-- this line will cause compilation error
+    }
+}
