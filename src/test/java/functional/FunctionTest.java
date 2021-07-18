e8a6bfddb89d8e4c40caf29a9db2fd8cdd79df71
kelvinleong <kelvin_liang@outlook.com>
2021-07-18 12:50:07 +0800

    [functional practice] -map chain refinement

--- src/test/java/functional/FunctionTest.java
+++ src/test/java/functional/FunctionTest.java
@@ -67,67 +67,59 @@ public class FunctionTest {
         assertTrue(set.contains("B"));
     }
 
+    private int getPriorityOrDefault(Optional<Integer> ...optionals) {
+        return Stream.of(optionals)
+                .flatMap(Optional::stream)
+                .filter(v -> v > 0)
+                .findFirst()
+                .orElse(10);
+    }
+
     @Test
     public void testPriorityMapChain() {
         // case0
         var constant = Constant.builder().build();
-        var v1 = Optional.ofNullable(constant.getPriority1() == 0 ? null : constant.getPriority1());
-        var v2 = Optional.ofNullable(constant.getPriority2() == 0 ? null : constant.getPriority2());
-        var v3 = Optional.ofNullable(constant.getPriority3() == 0 ? null : constant.getPriority3());
-        var v4 = Optional.ofNullable(constant.getPriority4() == 0 ? null : constant.getPriority4());
-        var r = Stream.of(v1, v2, v3, v4)
-                .flatMap(Optional::stream)
-                .findFirst()
-                .orElse(10);
+        var v1 = Optional.of(constant.getPriority1());
+        var v2 = Optional.of(constant.getPriority2());
+        var v3 = Optional.of(constant.getPriority3());
+        var v4 = Optional.of(constant.getPriority4());
+        var r = getPriorityOrDefault(v1, v2, v3, v4);
         assertEquals(10, r);
 
         // case1
         constant = Constant.builder().priority1(1).build();
-        v1 = Optional.ofNullable(constant.getPriority1() == 0 ? null : constant.getPriority1());
-        v2 = Optional.ofNullable(constant.getPriority2() == 0 ? null : constant.getPriority2());
-        v3 = Optional.ofNullable(constant.getPriority3() == 0 ? null : constant.getPriority3());
-        v4 = Optional.ofNullable(constant.getPriority4() == 0 ? null : constant.getPriority4());
-        r = Stream.of(v1, v2, v3, v4)
-                .flatMap(Optional::stream)
-                .findFirst()
-                .orElse(10);
+        v1 = Optional.of(constant.getPriority1());
+        v2 = Optional.of(constant.getPriority2());
+        v3 = Optional.of(constant.getPriority3());
+        v4 = Optional.of(constant.getPriority4());
+        r = getPriorityOrDefault(v1, v2, v3, v4);
         assertEquals(1, r);
 
         // case2
         constant = Constant.builder().priority2(2).build();
-        v1 = Optional.ofNullable(constant.getPriority1() == 0 ? null : constant.getPriority1());
-        v2 = Optional.ofNullable(constant.getPriority2() == 0 ? null : constant.getPriority2());
-        v3 = Optional.ofNullable(constant.getPriority3() == 0 ? null : constant.getPriority3());
-        v4 = Optional.ofNullable(constant.getPriority4() == 0 ? null : constant.getPriority4());
-        r = Stream.of(v1, v2, v3, v4)
-                .flatMap(Optional::stream)
-                .findFirst()
-                .orElse(10);
+        v1 = Optional.of(constant.getPriority1());
+        v2 = Optional.of(constant.getPriority2());
+        v3 = Optional.of(constant.getPriority3());
+        v4 = Optional.of(constant.getPriority4());
+        r = getPriorityOrDefault(v1, v2, v3, v4);
         assertEquals(2, r);
 
         // case3
         constant = Constant.builder().priority3(3).build();
-        v1 = Optional.ofNullable(constant.getPriority1() == 0 ? null : constant.getPriority1());
-        v2 = Optional.ofNullable(constant.getPriority2() == 0 ? null : constant.getPriority2());
-        v3 = Optional.ofNullable(constant.getPriority3() == 0 ? null : constant.getPriority3());
-        v4 = Optional.ofNullable(constant.getPriority4() == 0 ? null : constant.getPriority4());
-        r = Stream.of(v1, v2, v3, v4)
-                .flatMap(Optional::stream)
-                .findFirst()
-                .orElse(10);
+        v1 = Optional.of(constant.getPriority1());
+        v2 = Optional.of(constant.getPriority2());
+        v3 = Optional.of(constant.getPriority3());
+        v4 = Optional.of(constant.getPriority4());
+        r = getPriorityOrDefault(v1, v2, v3, v4);
         assertEquals(3, r);
 
         // case4
         constant = Constant.builder().priority4(4).build();
-        v1 = Optional.ofNullable(constant.getPriority1() == 0 ? null : constant.getPriority1());
-        v2 = Optional.ofNullable(constant.getPriority2() == 0 ? null : constant.getPriority2());
-        v3 = Optional.ofNullable(constant.getPriority3() == 0 ? null : constant.getPriority3());
-        v4 = Optional.ofNullable(constant.getPriority4() == 0 ? null : constant.getPriority4());
-        r = Stream.of(v1, v2, v3, v4)
-                .flatMap(Optional::stream)
-                .findFirst()
-                .orElse(10);
+        v1 = Optional.of(constant.getPriority1());
+        v2 = Optional.of(constant.getPriority2());
+        v3 = Optional.of(constant.getPriority3());
+        v4 = Optional.of(constant.getPriority4());
+        r = getPriorityOrDefault(v1, v2, v3, v4);
         assertEquals(4, r);
-
     }
 }
